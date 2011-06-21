d3e5cc997edee2de44a23c71846e7eda5895ddae
Michel Albert <michel@albert.lu>
2011-06-21 16:52:08 +0200

    Added license info and some ignore defs

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ from setuptools import setup, find_packages
 setup(
     name = "Wicked Jukebox Database",
     version = "1.0",
+    license = "BSD 3-Clause",
     packages = find_packages(),
+    long_description=open("README.rst").read(),
 
     install_requires = [
         'sqlalchemy==0.7',
